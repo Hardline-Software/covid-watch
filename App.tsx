b5381ff9fa4ecb9dec47bcf55292f5b42203aca4
Gent Semaj <gentsemaj@live.com>
2020-08-08 14:40:48 -0400

    Prepare main app container

--- App.tsx
+++ App.tsx
@@ -1,21 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import 'react-native-gesture-handler'
+import { NavigationContainer } from '@react-navigation/native'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import LogInPage from './pages/LogInPage'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <LogInPage />
+      </NavigationContainer>
       <StatusBar style="auto" />
-    </View>
-  );
+    </SafeAreaProvider>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ pages/LogInPage.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { StyleSheet, Image, Text, View } from 'react-native'
+import { TextInput } from 'react-native-gesture-handler'
+
+const LogInPage = () => {
+  return (
+    <View style={styles.container}>
+      <Image source={require('../assets/icon.png')} style={styles.icon} />
+      <View>
+        <View>
+          <TextInput></TextInput>
+        </View>
+        <View>
+          <Text>Sign Up</Text>
+        </View>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  icon: {
+    width: 120,
+    height: 120
+  }
+})
+
+export default LogInPage
