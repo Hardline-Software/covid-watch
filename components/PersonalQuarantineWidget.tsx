d25b912a19a351f446935603315d7ecde5edba82
Gent Semaj <gentsemaj@live.com>
2020-08-09 23:06:01 -0400

    Fix date issue

--- components/PersonalQuarantineWidget.tsx
+++ components/PersonalQuarantineWidget.tsx
@@ -2,29 +2,63 @@ import React from 'react'
 import Widget from './Widget'
 import { StyleSheet, Text, View } from 'react-native'
 import { useAuthUser } from '../hooks/useAuthUser'
-import { useQuarantineQuery } from '../generated/graphql'
+import {
+  useUserQuarantinesQuery,
+  useCreateQuarantineMutation,
+  UserQuarantinesDocument,
+  UserQuarantinesQuery,
+  UserQuarantinesQueryVariables
+} from '../generated/graphql'
 import TextButton from './TextButton'
 import { MaterialIcons } from '@expo/vector-icons'
 
 const PersonalQuarantine = () => {
   const { user } = useAuthUser()
 
-  const { data, loading, error } = useQuarantineQuery({
+  const { data, loading, error } = useUserQuarantinesQuery({
     variables: {
-      id: user?.id!
+      userId: user?.id!
     },
     skip: !user
+  })
+
+  const [createQuarantine] = useCreateQuarantineMutation({
+    update: (cache, { data }) => {
+      if (data) {
+        const usersQuery = cache.readQuery<UserQuarantinesQuery, UserQuarantinesQueryVariables>({
+          query: UserQuarantinesDocument,
+          variables: {
+            userId: data?.createQuarantine?.userId!
+          }
+        })
+        if (usersQuery) {
+          cache.writeQuery<UserQuarantinesQuery, UserQuarantinesQueryVariables>({
+            query: UserQuarantinesDocument,
+            variables: {
+              userId: data?.createQuarantine?.userId!
+            },
+            data: {
+              ...usersQuery,
+              userQuarantines: {
+                ...usersQuery.userQuarantines,
+                items: [...usersQuery!.userQuarantines!.items!, data.createQuarantine!]
+              }
+            }
+          })
+        }
+      }
+    }
   })
 
   return (
     <Widget
       title="Personal Quarantine"
-      expandable={data?.quarantine ? true : false}
+      expandable={data?.userQuarantines?.items ? true : false}
       base={
-        data?.quarantine ? (
+        data?.userQuarantines ? (
           <>
             <View style={{ ...styles.container, backgroundColor: '#b5ffc3' }}>
-              <Text style={styles.big}>0</Text>
+              <Text style={styles.big}>{(new Date().getTime() - new Date().getTime()) / 86400000}</Text>
               <Text>days in quarantine</Text>
             </View>
             <TextButton
